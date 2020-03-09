79ee671abf225bac61acf4ad78a497d88a46fc89
RickyYXY <yuxingyuan@gmail.com>
2020-03-09 16:33:44 +0800

    Homework4

---
+++ Homework4/Clock/Clock/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Clock
+{
+    class Program
+    {
+        static void OnTick1(ClockEventArgs args)
+        {
+            Console.WriteLine($"Tick: {args.Minute}:{args.Second}");
+        }
+
+        static void OnAlarm1(ClockEventArgs args)
+        {
+            Console.WriteLine("It's time!\a");
+            Console.WriteLine($"{args.Minute}:{args.Second}");
+        }
+
+        static void Main(string[] args)
+        {
+            Clock clk1 = new Clock(0, 10);
+            clk1.OnTick += OnTick1;
+            clk1.OnAlarm += OnAlarm1;
+            clk1.Start();
+        }
+    }
+
+    delegate void TickHandler(ClockEventArgs args);
+    delegate void AlarmHandler(ClockEventArgs args);
+
+    class ClockEventArgs
+    {
+        public int Minute { get; set; }
+        public int Second { get; set; }
+    }
+    class Clock
+    {
+        private int alarmMin, alarmSec;
+        public event TickHandler OnTick;
+        public event AlarmHandler OnAlarm;
+        public void Start()
+        {
+            int min = 0, sec = 0;
+            while (min < 2)
+            {
+                Thread.Sleep(1000);
+                min = (min + (sec + 1) / 60) % 60;
+                sec = (sec + 1) % 60;
+                if(min == alarmMin && sec == alarmSec)
+                {
+                    ClockEventArgs arg2 = new ClockEventArgs() { Minute = min, Second = sec };
+                    OnAlarm(arg2);
+                }
+                else
+                {
+                    ClockEventArgs arg1 = new ClockEventArgs() { Minute = min, Second = sec };
+                    OnTick(arg1);
+                }
+            }
+        }
+        public Clock(int min, int sec) { alarmMin = min;alarmSec = sec; }
+    }
+    
+}
---
+++ Homework4/List/List/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkList<int> intlist = new LinkList<int>();
+            for (int i = 1; i <= 10; i++)
+                intlist.Add(i);
+            //print
+            intlist.ForEach(p => Console.WriteLine($"Node data:{p.Data}"));
+            //get max
+            int max = Int32.MinValue;
+            intlist.ForEach(p => max = p.Data > max ? p.Data : max);
+            Console.WriteLine($"Max:{max}");
+            int min = Int32.MaxValue;
+            intlist.ForEach(p => min = p.Data < min ? p.Data : min);
+            Console.WriteLine($"Min:{min}");
+            int sum = 0;
+            intlist.ForEach(p => sum += p.Data);
+            Console.WriteLine($"Sum:{sum}");
+
+        }
+    }
+    class LinkNode<T>
+    {
+        public LinkNode<T> Next { get; set; }
+        public T Data { get; set; }
+        public LinkNode(T t)
+        {
+            Next = null;
+            Data = t;
+        }   
+    }
+    class LinkList<T>
+    {
+        private LinkNode<T> head, tail;
+        public LinkList()
+        {
+            head = tail = null;
+        }
+        public void Add(T t)
+        {
+            LinkNode<T> node = new LinkNode<T>(t);
+            if (tail == null)
+                head = tail = node;
+            else
+            {
+                tail.Next = node;
+                tail = node;
+            }
+        }
+        public void ForEach(Action<LinkNode<T>> action)
+        {
+            LinkNode<T> p=head;
+            while(p!=null)
+            {
+                action(p);
+                p = p.Next;
+            }
+        }
+    }
+}
