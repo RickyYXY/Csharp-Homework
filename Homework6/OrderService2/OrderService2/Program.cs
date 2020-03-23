2796168f19c104dbb51c41a9e492508be9a9616e
RickyYXY <yuxingyuan@gmail.com>
2020-03-23 17:28:53 +0800

    Homework6

---
+++ Homework6/OrderService2/OrderService2/Program.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace OrderService2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OrderService os = new OrderService();
+            Random rd = new Random();
+            while (true)
+            {
+                Console.WriteLine("A ---> Add an order\n"
+                    + "D ---> Delete an order\n"
+                    + "S ---> Sort your order\n"
+                    + "Q ---> Do a query\n"
+                    + "P ---> Print all order\n"
+                    + "E ---> End the service");
+                string cmd = Console.ReadLine();
+                try
+                {
+                    switch (cmd)
+                    {
+                        case "A":
+                            List<OrderItem> items = new List<OrderItem>();
+                            //string customer;
+                            Console.WriteLine("Your name: ");
+                            string customer = Console.ReadLine();
+                            while (cmd != "N")
+                            {
+                                string itname, quantity, price;
+                                Console.WriteLine("Product's name: ");
+                                itname = Console.ReadLine();
+                                Console.WriteLine("Product's quantity: ");
+                                quantity = Console.ReadLine();
+                                Console.WriteLine("Product's unit price:");
+                                price = Console.ReadLine();
+                                items.Add(new OrderItem(itname, Int32.Parse(quantity), Double.Parse(price)));
+                                Console.WriteLine("Finsh. Continue? (Y/N): ");
+                                cmd = Console.ReadLine();
+                            }
+                            int id = rd.Next(1, 100);
+                            Order order = new Order(customer, id, items);
+                            while (!os.AddOrder(order))
+                            {
+                                id = rd.Next(1, 100);
+                                order.ID = id;
+                            }
+                            Console.WriteLine("Order created.\n" + order.ToString());
+                            break;
+                        case "D":
+                            Console.WriteLine("Order's ID: ");
+                            string idnum = Console.ReadLine();
+                            if (os.DeleteOrder(Int32.Parse(idnum)))
+                                Console.WriteLine("Delete successfully.\n");
+                            else
+                                Console.WriteLine("Can't find the order.\n");
+                            break;
+                        case "S":
+                            os.OrderSort();
+                            Console.WriteLine("Finish.\n");
+                            break;
+                        case "Q":
+                            Console.WriteLine("ID: ");
+                            idnum = Console.ReadLine();
+                            List<Order> result = os.FindOrder(Int32.Parse(idnum));
+                            foreach (Order o in result)
+                                Console.WriteLine(o.ToString());
+                            break;
+                        case "P":
+                            os.PrintAllOrder();
+                            break;
+                        default:
+                            break;
+
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Wrong input format, try again please.");
+                }
+                if (cmd == "E")
+                    break;
+            }
+        }
+    }
+
+    [Serializable]
+    public class OrderItem
+    {
+
+        public string Name { set; get; }
+        public int Quantity { set; get; }
+        public double Price { set; get; }
+        public OrderItem() { }
+        public OrderItem(string name, int quantity, double price)
+        {
+            Name = name;
+            Quantity = quantity;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return "Item name: " + Name + "\nOrder num: " + Quantity.ToString()
+                    + "\nItem price: " + Price.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var item = obj as OrderItem;
+            return item != null &&
+                   Name == item.Name &&
+                   Quantity == item.Quantity &&
+                   Price == item.Price;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 1303464505;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Name);
+            hashCode = hashCode * -1521134295 + Quantity.GetHashCode();
+            hashCode = hashCode * -1521134295 + Price.GetHashCode();
+            return hashCode;
+        }
+    }
+
+    [Serializable]
+    public class Order
+    {
+        public string Customer { set; get; }
+        public int ID { set; get; }
+        public List<OrderItem> items;
+        public Order() { items = new List<OrderItem>(); }
+        public Order(string customer, int id, List<OrderItem> item)
+        {
+            Customer = customer;
+            ID = id;
+            this.items = item;
+        }
+        public double SumCost()
+        {
+            double sum = 0;
+            foreach (OrderItem item in items)
+                sum += item.Price * item.Quantity;
+            return sum;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder itemlist = new StringBuilder(20);
+            for (int i = 0; i < items.ToArray().Length; i++)
+                itemlist.Append(items[i].ToString() + "\n");
+            return "Client's name: " + Customer +
+                   "\nOrder's ID: " + ID +
+                   "\n" + itemlist;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var order = obj as Order;
+            return order != null &&
+                   ID == order.ID;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 1162405519;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Customer);
+            hashCode = hashCode * -1521134295 + ID.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<List<OrderItem>>.Default.GetHashCode(items);
+            return hashCode;
+        }
+    }
+
+    public class OrderService
+    {
+        private List<Order> orders;
+        public OrderService() { orders = new List<Order>(); }
+        public bool AddOrder(Order order)
+        {
+            if (orders.Contains(order))
+                return false;
+            orders.Add(order);
+            return true;
+        }
+        public bool UpdateOrder(int id, Order order)
+        {
+            int index = orders.FindIndex(o => o.ID == id);
+            if (index == -1)
+                return false;
+            orders[index] = order;
+            return true;
+        }
+        public bool Clean()
+        {
+            orders.Clear();
+            return orders.ToArray().Length == 0;
+        }
+        public bool DeleteOrder(int id)
+        {
+            int index = orders.FindIndex(o => o.ID == id);
+            if (index == -1)
+                return false;
+            orders.RemoveAt(index);
+            return true;
+        }
+        public List<Order> FindOrder(Func<Order, bool> action)
+        {
+            var query = orders.Where(action).OrderBy(order => order.SumCost());
+            if (query.Count() == 0)
+                return null;
+            return query.ToList();
+        }
+        public List<Order> FindOrder(int id)
+        {
+            var query = orders.Where(o => o.ID == id).OrderBy(order => order.SumCost());
+            if (query.Count() == 0)
+                return null;
+            return query.ToList();
+        }
+        public List<Order> FindOrder(string input, string key)
+        {
+            if(key=="c") //customer search
+            {
+                var query = orders.Where(o => o.Customer == input)
+                    .OrderBy(order => order.SumCost());
+                if (query.Count() == 0)
+                    return null;
+                return query.ToList();
+            }
+            else //itemname search
+            {
+                var query = orders.Where(o => o.items.Exists(item => item.Name == input))
+                                .OrderBy(order => order.SumCost());
+                if (query.Count() == 0)
+                    return null;
+                return query.ToList();
+            }
+        }
+        public void OrderSort()  //default sort
+        {
+            orders.Sort((o1, o2) => o1.ID - o2.ID);
+        }
+        public void OrderSort(Func<Order, Order, int> func)
+        {
+            orders.Sort((o1, o2) => func(o1, o2));
+        }
+        public void PrintAllOrder()
+        {
+            for (int i = 0; i < orders.ToArray().Length; i++)
+                Console.WriteLine(orders[i].ToString() + "\n");
+        }
+        public List<Order> GetAllOrders()
+        {
+            return orders;
+        }
+        public void ExportOrders(string filename)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Order>));
+            using (FileStream fout = new FileStream(filename, FileMode.Create))
+            {
+                xmlSerializer.Serialize(fout, orders);
+            }
+        }
+        public void ImportOrders(string filename)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Order>));
+            using (FileStream fin = new FileStream(filename, FileMode.OpenOrCreate))
+            {
+                orders = (List<Order>)xmlSerializer.Deserialize(fin);
+            }
+        }
+    }
+}
---
+++ Homework6/OrderService2/OrderService2Tests/OrderServiceTests.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OrderService2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace OrderService2.Tests
+{
+    [TestClass()]
+    public class OrderServiceTests
+    {
+        private OrderService os = new OrderService();
+        private List<Order> orders = new List<Order>();
+        //string[] customers = { "Ricky", "Melissa" };
+        //int[] ids = { 518, 1125 };
+        //string[][] itemNames = new string[2][]{ new string[]{ "iPad", "Textbook" },
+        //                                new string[]{"Pen","Flower" } };
+        //int[][] quantities = new int[2][] { new int[] { 1, 5 }, new int[] { 3, 10 } };
+        //double[][] prices = new double[2][] { new double[] { 1999, 25 }, new double[] { 199, 15 } };
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            string[] customers = { "Ricky", "Melissa" };
+            int[] ids = { 518, 1125 };
+            string[][] itemNames = new string[2][]{ new string[]{ "iPad", "Textbook" },
+                                        new string[]{"Pen","Flower" } };
+            int[][] quantities = new int[2][] { new int[] { 1, 5 }, new int[] { 3, 10 } };
+            double[][] prices = new double[2][] { new double[] { 1999, 25 }, new double[] { 199, 15 } };
+            for (int i = 0; i < ids.Length; i++)
+            {
+                List<OrderItem> items = new List<OrderItem>();
+                for (int j = 0; j < itemNames[i].Length; j++)
+                {
+                    OrderItem item = new OrderItem(itemNames[i][j], quantities[i][j], prices[i][j]);
+                    items.Add(item);
+                }
+                Order order = new Order(customers[i], ids[i], items);
+                orders.Add(order);
+            }
+            for (int i = 0; i < orders.ToArray().Length; i++)
+                os.AddOrder(orders[i]);
+        }
+        [TestCleanup]
+        public void CleanUp()
+        {
+            os.Clean();
+            orders.Clear();
+        }
+
+        [TestMethod()]
+        public void ExportOrdersTest()
+        {
+            os.ExportOrders("orderlist.xml");
+            FileInfo fileInfo = new FileInfo("orderlist.xml");
+            Assert.IsTrue(fileInfo.Exists);
+        }
+
+        [TestMethod()]
+        public void ImportOrdersTest()
+        {
+            OrderService os2 = new OrderService();
+            os2.ImportOrders("orderlist.xml");
+            List<Order> orders2 = os2.GetAllOrders();
+            //Assert.AreEqual(orders, orders2);
+            for (int i = 0; i < orders.ToArray().Length; i++)
+            {
+                Assert.AreEqual(orders[i], orders2[i]);
+                for (int j = 0; j < orders[i].items.ToArray().Length; j++)
+                    Assert.AreEqual(orders[i].items[j], orders2[i].items[j]);
+            }
+        }
+
+        [TestMethod()]
+        public void AddOrderTest()
+        {
+            List<OrderItem> oi2 = new List<OrderItem>();
+            oi2.Add(new OrderItem("toy", 5, 10));
+            Order order2 = new Order("A", 1, oi2);
+            os.AddOrder(order2);
+            List<Order> ol2 = os.FindOrder(1);
+            Assert.AreEqual(ol2[0], order2);
+            for (int j = 0; j < ol2[0].items.ToArray().Length; j++)
+                Assert.AreEqual(ol2[0].items[j], order2.items[j]);
+        }
+
+        [TestMethod()]
+        public void FindOrderTest()
+        {
+            List<Order> ol2 = os.FindOrder(1125);
+            Assert.AreEqual(ol2[0], orders[1]);
+
+            ol2 = os.FindOrder("Ricky", "c");
+            Assert.AreEqual(ol2[0], orders[0]);
+
+            ol2 = os.FindOrder("Flower", "i");
+            Assert.AreEqual(ol2[0], orders[1]);
+        }
+
+        [TestMethod()]
+        public void DeleteOrderTest()
+        {
+            os.DeleteOrder(518);
+            Assert.IsNull(os.FindOrder(518));
+        }
+
+        [TestMethod()]
+        public void OrderSortTest()
+        {
+            orders.Sort((o1, o2) => o1.ID - o2.ID);
+            os.OrderSort();
+            List<Order> ol2 = os.GetAllOrders();
+            for (int i = 0; i < orders.ToArray().Length; i++)
+                Assert.AreEqual(orders[i], ol2[i]);
+        }
+    }
+}
