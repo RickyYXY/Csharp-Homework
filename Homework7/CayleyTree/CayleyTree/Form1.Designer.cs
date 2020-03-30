370c76ad2b3220f2bb8136c19a03d57919439e46
RickyYXY <yuxingyuan@gmail.com>
2020-03-30 17:23:00 +0800

    cayleytree1

---
+++ Homework7/CayleyTree/CayleyTree/Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace CayleyTree
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.textBox_RightTH = new System.Windows.Forms.TextBox();
+            this.textBox_LeftTH = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.trackBar_Right = new System.Windows.Forms.TrackBar();
+            this.trackBar_Left = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar_Depth = new System.Windows.Forms.TrackBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_Right)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_Left)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_Depth)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.radioButton3);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Controls.Add(this.textBox_RightTH);
+            this.panel1.Controls.Add(this.textBox_LeftTH);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.trackBar_Right);
+            this.panel1.Controls.Add(this.trackBar_Left);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.trackBar_Depth);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(496, 142);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(397, 232);
+            this.panel1.TabIndex = 0;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.Info;
+            this.label7.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label7.Location = new System.Drawing.Point(253, 26);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(91, 19);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Your color:";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.radioButton3.Location = new System.Drawing.Point(257, 111);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(78, 24);
+            this.radioButton3.TabIndex = 15;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Yellow";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Checked = true;
+            this.radioButton2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.radioButton2.Location = new System.Drawing.Point(257, 85);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(59, 24);
+            this.radioButton2.TabIndex = 14;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Red";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.radioButton1.Location = new System.Drawing.Point(257, 59);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(60, 24);
+            this.radioButton1.TabIndex = 13;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Pink";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // textBox_RightTH
+            // 
+            this.textBox_RightTH.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox_RightTH.Location = new System.Drawing.Point(130, 197);
+            this.textBox_RightTH.Name = "textBox_RightTH";
+            this.textBox_RightTH.Size = new System.Drawing.Size(62, 27);
+            this.textBox_RightTH.TabIndex = 12;
+            this.textBox_RightTH.Text = "20";
+            this.textBox_RightTH.TextChanged += new System.EventHandler(this.textBox_RightTH_TextChanged);
+            // 
+            // textBox_LeftTH
+            // 
+            this.textBox_LeftTH.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox_LeftTH.Location = new System.Drawing.Point(130, 163);
+            this.textBox_LeftTH.Name = "textBox_LeftTH";
+            this.textBox_LeftTH.Size = new System.Drawing.Size(62, 27);
+            this.textBox_LeftTH.TabIndex = 11;
+            this.textBox_LeftTH.Text = "30";
+            this.textBox_LeftTH.TextChanged += new System.EventHandler(this.textBox_LeftTH_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.Info;
+            this.label6.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label6.Location = new System.Drawing.Point(41, 197);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(73, 19);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Right th:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.Info;
+            this.label5.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(52, 163);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 19);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Left th:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // trackBar_Right
+            // 
+            this.trackBar_Right.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.trackBar_Right.Location = new System.Drawing.Point(120, 126);
+            this.trackBar_Right.Name = "trackBar_Right";
+            this.trackBar_Right.Size = new System.Drawing.Size(104, 56);
+            this.trackBar_Right.TabIndex = 8;
+            this.trackBar_Right.Value = 7;
+            this.trackBar_Right.ValueChanged += new System.EventHandler(this.trackBar_Right_ValueChanged);
+            // 
+            // trackBar_Left
+            // 
+            this.trackBar_Left.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.trackBar_Left.Location = new System.Drawing.Point(120, 88);
+            this.trackBar_Left.Name = "trackBar_Left";
+            this.trackBar_Left.Size = new System.Drawing.Size(104, 56);
+            this.trackBar_Left.TabIndex = 7;
+            this.trackBar_Left.Value = 6;
+            this.trackBar_Left.ValueChanged += new System.EventHandler(this.trackBar_Left_ValueChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.Info;
+            this.label4.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(32, 126);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 19);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Right per:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Info;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(43, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 19);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Left per:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(130, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(62, 27);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "100";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Info;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(16, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Branch Len:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Info;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(16, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Your depth:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // trackBar_Depth
+            // 
+            this.trackBar_Depth.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.trackBar_Depth.Location = new System.Drawing.Point(120, 16);
+            this.trackBar_Depth.Name = "trackBar_Depth";
+            this.trackBar_Depth.Size = new System.Drawing.Size(104, 56);
+            this.trackBar_Depth.TabIndex = 1;
+            this.trackBar_Depth.Value = 7;
+            this.trackBar_Depth.ValueChanged += new System.EventHandler(this.trackBar_Depth_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Tempus Sans ITC", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(257, 164);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Do it!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Location = new System.Drawing.Point(33, 66);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(419, 443);
+            this.panel2.TabIndex = 1;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.SystemColors.Info;
+            this.label8.Font = new System.Drawing.Font("微软雅黑", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label8.Location = new System.Drawing.Point(28, 28);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(105, 26);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "Your tree:";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(895, 558);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "CayleyTree";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_Right)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_Left)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_Depth)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar_Depth;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TrackBar trackBar_Right;
+        private System.Windows.Forms.TrackBar trackBar_Left;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox_RightTH;
+        private System.Windows.Forms.TextBox textBox_LeftTH;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Homework7/CayleyTree/CayleyTree/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CayleyTree
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        // parameters
+        private Graphics graphics;
+        double th1 = 30 * Math.PI / 180;
+        double th2 = 20 * Math.PI / 180;
+        double per1 = 0.6;
+        double per2 = 0.7;
+        Color red = Color.Red;
+        Color pink = Color.FromArgb(255, 0, 255);
+        Color yellow = Color.FromArgb(255, 215, 0);
+        int draw_depth = 10;
+        double leng = 100;
+        Pen pen = new Pen(Color.Red);
+
+        private void drawLine(double x0,double y0,double x1,double y1)
+        {
+            graphics.DrawLine(
+                pen, (int)x0, (int)y0, (int)x1, (int)y1);
+        }
+
+        private void drawCayleyTree(int n, double x0, double y0,
+            double leng, double th)
+        {
+            if (n == 0) return;
+
+            double x1 = x0 + leng * Math.Cos(th);
+            double y1 = y0 + leng * Math.Sin(th);
+
+            drawLine(x0, y0, x1, y1);
+
+            drawCayleyTree(n - 1, x1, y1, per1 * leng, th + th1);
+            drawCayleyTree(n - 1, x1, y1, per2 * leng, th - th2);
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (graphics == null)
+                graphics = this.panel2.CreateGraphics();
+            graphics.Clear(panel2.BackColor);
+            drawCayleyTree(draw_depth, panel2.Width/2, panel2.Height,
+                leng, -Math.PI / 2);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            leng = Double.Parse(textBox1.Text);
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            pen.Color = pink;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            pen.Color = red;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            pen.Color = yellow;
+        }
+
+        private void trackBar_Depth_ValueChanged(object sender, EventArgs e)
+        {
+            draw_depth = trackBar_Depth.Value*15 / trackBar_Depth.Maximum;
+        }
+
+        private void trackBar_Left_ValueChanged(object sender, EventArgs e)
+        {
+            per1 = trackBar_Left.Value / (double)trackBar_Left.Maximum;
+        }
+
+        private void trackBar_Right_ValueChanged(object sender, EventArgs e)
+        {
+            per2 = trackBar_Right.Value / (double)trackBar_Right.Maximum;
+        }
+
+        private void textBox_LeftTH_TextChanged(object sender, EventArgs e)
+        {
+            th1 = Double.Parse(textBox_LeftTH.Text)* Math.PI / 180;
+        }
+
+        private void textBox_RightTH_TextChanged(object sender, EventArgs e)
+        {
+            th2 = Double.Parse(textBox_RightTH.Text)* Math.PI / 180;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
