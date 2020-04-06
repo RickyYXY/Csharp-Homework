b2e5c1448fa08d23ffeb7861f6d67726f5e0374c
RickyYXY <yuxingyuan@gmail.com>
2020-04-06 22:32:13 +0800

    3

--- Homework8/OrderServiceForm/OrderServiceForm/Form4.Designer.cs
+++ Homework8/OrderServiceForm/OrderServiceForm/Form4.Designer.cs
@@ -55,12 +55,10 @@
             // radioButton3
             // 
             this.radioButton3.AutoSize = true;
-            this.radioButton3.Checked = true;
             this.radioButton3.Location = new System.Drawing.Point(6, 76);
             this.radioButton3.Name = "radioButton3";
             this.radioButton3.Size = new System.Drawing.Size(118, 19);
             this.radioButton3.TabIndex = 5;
-            this.radioButton3.TabStop = true;
             this.radioButton3.Text = "默认排序查询";
             this.radioButton3.UseVisualStyleBackColor = true;
             this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
@@ -72,7 +70,6 @@
             this.radioButton2.Name = "radioButton2";
             this.radioButton2.Size = new System.Drawing.Size(133, 19);
             this.radioButton2.TabIndex = 4;
-            this.radioButton2.TabStop = true;
             this.radioButton2.Text = "以订单客户查询";
             this.radioButton2.UseVisualStyleBackColor = true;
             this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
@@ -126,6 +123,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
+            this.Name = "Form4";
             this.Text = "查询订单";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
