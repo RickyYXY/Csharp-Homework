4a72fd87fee6606542a4458722ccf2b276d588ce
RickyYXY <yuxingyuan@gmail.com>
2020-05-11 22:49:28 +0800

    get post

--- Homework12/WebOrderService/WebOrderService/Controllers/OrderController.cs
+++ Homework12/WebOrderService/WebOrderService/Controllers/OrderController.cs
@@ -21,8 +21,9 @@ namespace WebOrderService.Controllers
 
         private IQueryable<Order> AllOrders()
         {
-            return orderContext.Orders.Include(o => o.Items.Select(i => i.GoodsItem))
-                .Include("Customer");
+            return orderContext.Orders.Include(o => o.Items)
+                .ThenInclude(i => i.GoodsItem)
+                .Include(o => o.Customer);
         }
 
         //GET: api/order
@@ -40,6 +41,49 @@ namespace WebOrderService.Controllers
             var order = AllOrders().FirstOrDefault(o => o.Id == id);
             if (order == null)
                 return NotFound();
+            return order;
+        }
+
+        //GET: api/order/goods?name=
+        [HttpGet("goods")]
+        public ActionResult<List<Order>>GetOrderByGoodsName(string goodsName)
+        {
+            var orders = AllOrders()
+          .Where(o => o.Items.Count(i => i.GoodsItem.Name == goodsName) > 0);
+            return orders.ToList();
+        }
+
+        //GET: api/order/customer?name=
+        [HttpGet("customer")]
+        public ActionResult<List<Order>>GetOrderByCustomerName(string customerName)
+        {
+            var orders = AllOrders()
+          .Where(o => o.Customer.Name == customerName);
+            return orders.ToList();
+        }
+
+        //Get: api/order/total?total=
+        [HttpGet("total")]
+        public ActionResult<List<Order>>GetOrderByTotalAmount(double total)
+        {
+            var orders = AllOrders()
+          .Where(o => o.Items.Sum(item => item.GoodsItem.Price * item.Quantity) > total);
+            return orders.ToList();
+        }
+
+        //POST: api/order
+        [HttpPost]
+        public ActionResult<Order>AddOrder(Order order)
+        {
+            try
+            {
+                orderContext.Orders.Add(order);
+                orderContext.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.InnerException.Message);
+            }
             return order;
         }
     }
