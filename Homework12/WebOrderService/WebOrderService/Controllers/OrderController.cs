1f86c993d1d78dac10f79c7cff23c49106f9c902
RickyYXY <yuxingyuan@gmail.com>
2020-05-11 21:21:25 +0800

    1

---
+++ Homework12/WebOrderService/WebOrderService/Controllers/OrderController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebOrderService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderController : Controller
+    {
+        private readonly OrderContext orderContext;
+
+        public OrderController(OrderContext orderContext)
+        {
+            this.orderContext = orderContext;
+        }
+
+        private IQueryable<Order> AllOrders()
+        {
+            return orderContext.Orders.Include(o => o.Items.Select(i => i.GoodsItem))
+                .Include("Customer");
+        }
+
+        //GET: api/order
+        [HttpGet]
+        public ActionResult<List<Order>>GetAllOrders()
+        {
+            var orders = AllOrders();
+            return orders.ToList();
+        }
+
+        //GET: api/order/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Order>GetOrderByID(string id)
+        {
+            var order = AllOrders().FirstOrDefault(o => o.Id == id);
+            if (order == null)
+                return NotFound();
+            return order;
+        }
+    }
+}
